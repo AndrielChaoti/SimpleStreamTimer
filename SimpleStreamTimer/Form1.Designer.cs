c26d64385733720f21ddea97e0ad518a5ec456f0
Andriel Chaoti <accounts@vandesdelca32.net>
2018-10-09 17:51:56 -0600

    First code commit.

---
+++ SimpleStreamTimer/Form1.Designer.cs
@@ -0,0 +1,314 @@
+namespace SimpleStreamTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.grpOptions = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.chkShowTray = new System.Windows.Forms.CheckBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numHours = new System.Windows.Forms.NumericUpDown();
+            this.numMinutes = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numSeconds = new System.Windows.Forms.NumericUpDown();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkShowHours = new System.Windows.Forms.CheckBox();
+            this.grpOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Simple Timer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // grpOptions
+            // 
+            this.grpOptions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpOptions.Controls.Add(this.label1);
+            this.grpOptions.Controls.Add(this.txtFilePath);
+            this.grpOptions.Controls.Add(this.btnBrowse);
+            this.grpOptions.Controls.Add(this.chkShowTray);
+            this.grpOptions.Controls.Add(this.btnStop);
+            this.grpOptions.Controls.Add(this.btnStart);
+            this.grpOptions.Location = new System.Drawing.Point(12, 69);
+            this.grpOptions.Name = "grpOptions";
+            this.grpOptions.Size = new System.Drawing.Size(378, 83);
+            this.grpOptions.TabIndex = 0;
+            this.grpOptions.TabStop = false;
+            this.grpOptions.Text = "Options";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path:";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SimpleStreamTimer.Properties.Settings.Default, "filepath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtFilePath.Location = new System.Drawing.Point(63, 21);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(277, 20);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.Text = global::SimpleStreamTimer.Properties.Settings.Default.filepath;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnBrowse.AutoSize = true;
+            this.btnBrowse.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnBrowse.Location = new System.Drawing.Point(346, 19);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(26, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // chkShowTray
+            // 
+            this.chkShowTray.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkShowTray.AutoSize = true;
+            this.chkShowTray.Checked = global::SimpleStreamTimer.Properties.Settings.Default.show_in_tray;
+            this.chkShowTray.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowTray.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::SimpleStreamTimer.Properties.Settings.Default, "show_in_tray", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.chkShowTray.Location = new System.Drawing.Point(6, 60);
+            this.chkShowTray.Name = "chkShowTray";
+            this.chkShowTray.Size = new System.Drawing.Size(125, 17);
+            this.chkShowTray.TabIndex = 3;
+            this.chkShowTray.Text = "Show in System Tray";
+            this.chkShowTray.UseVisualStyleBackColor = true;
+            this.chkShowTray.CheckedChanged += new System.EventHandler(this.chkShowTray_CheckedChanged);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(216, 54);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(297, 54);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.FileName = "countdown.txt";
+            this.saveFileDialog1.Filter = "Text Files|*.txt|All Files|*.*\"";
+            this.saveFileDialog1.InitialDirectory = "%USERPROFILE%\\Desktop";
+            this.saveFileDialog1.OverwritePrompt = false;
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numHours
+            // 
+            this.numHours.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::SimpleStreamTimer.Properties.Settings.Default, "timer_hours", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.numHours.Location = new System.Drawing.Point(12, 25);
+            this.numHours.Name = "numHours";
+            this.numHours.Size = new System.Drawing.Size(120, 20);
+            this.numHours.TabIndex = 4;
+            this.numHours.Value = global::SimpleStreamTimer.Properties.Settings.Default.timer_hours;
+            this.numHours.ValueChanged += new System.EventHandler(this.num_ValueChanged);
+            // 
+            // numMinutes
+            // 
+            this.numMinutes.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::SimpleStreamTimer.Properties.Settings.Default, "timer_mins", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.numMinutes.Location = new System.Drawing.Point(138, 25);
+            this.numMinutes.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numMinutes.Name = "numMinutes";
+            this.numMinutes.Size = new System.Drawing.Size(120, 20);
+            this.numMinutes.TabIndex = 5;
+            this.numMinutes.Value = global::SimpleStreamTimer.Properties.Settings.Default.timer_mins;
+            this.numMinutes.ValueChanged += new System.EventHandler(this.num_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Minutes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(261, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Seconds:";
+            // 
+            // numSeconds
+            // 
+            this.numSeconds.DataBindings.Add(new System.Windows.Forms.Binding("Value", global::SimpleStreamTimer.Properties.Settings.Default, "timer_secs", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.numSeconds.Location = new System.Drawing.Point(264, 25);
+            this.numSeconds.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numSeconds.Name = "numSeconds";
+            this.numSeconds.Size = new System.Drawing.Size(120, 20);
+            this.numSeconds.TabIndex = 6;
+            this.numSeconds.Value = global::SimpleStreamTimer.Properties.Settings.Default.timer_secs;
+            this.numSeconds.ValueChanged += new System.EventHandler(this.num_ValueChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblTime.Location = new System.Drawing.Point(138, 48);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(252, 17);
+            this.lblTime.TabIndex = 8;
+            this.lblTime.Text = "timer";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hours:";
+            // 
+            // chkShowHours
+            // 
+            this.chkShowHours.AutoSize = true;
+            this.chkShowHours.Checked = global::SimpleStreamTimer.Properties.Settings.Default.show_hours;
+            this.chkShowHours.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowHours.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::SimpleStreamTimer.Properties.Settings.Default, "show_hours", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.chkShowHours.Location = new System.Drawing.Point(12, 48);
+            this.chkShowHours.Name = "chkShowHours";
+            this.chkShowHours.Size = new System.Drawing.Size(84, 17);
+            this.chkShowHours.TabIndex = 9;
+            this.chkShowHours.Text = "Show Hours";
+            this.chkShowHours.UseVisualStyleBackColor = true;
+            this.chkShowHours.CheckedChanged += new System.EventHandler(this.chkShowHours_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 164);
+            this.Controls.Add(this.chkShowHours);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.numSeconds);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numHours);
+            this.Controls.Add(this.grpOptions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Simple Timer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.grpOptions.ResumeLayout(false);
+            this.grpOptions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox grpOptions;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox chkShowTray;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.NumericUpDown numHours;
+        private System.Windows.Forms.NumericUpDown numMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numSeconds;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkShowHours;
+    }
+}
+
---
+++ SimpleStreamTimer/Form1.cs
@@ -0,0 +1,169 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace SimpleStreamTimer {
+    public partial class Form1 : Form {
+        DateTime countdownStarted = new DateTime();
+        TimeSpan countdown = new TimeSpan();
+        bool isCounting = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void chkShowTray_CheckedChanged(object sender, EventArgs e) {
+            notifyIcon1.Visible = chkShowTray.Checked;
+            //Properties.Settings.Default.show_in_tray = chkShowTray.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //chkShowTray.Checked = Properties.Settings.Default.show_in_tray;
+            notifyIcon1.Icon = Properties.Resources.clock_face;
+            notifyIcon1.Visible = chkShowTray.Checked;
+
+            string filepath = Properties.Settings.Default.filepath;
+            if (filepath == "") {
+                filepath = Environment.ExpandEnvironmentVariables("%userprofile%\\Desktop\\countdown.txt");
+            }
+            txtFilePath.Text = filepath;
+
+            //numHours.Value = Properties.Settings.Default.timer_hours;
+            //numMinutes.Value = Properties.Settings.Default.timer_mins;
+            //numSeconds.Value = Properties.Settings.Default.timer_secs;
+            countdown = TimeSpan.FromHours((double)numHours.Value) + TimeSpan.FromMinutes((double)numMinutes.Value) + TimeSpan.FromSeconds((double)numSeconds.Value);
+            if (chkShowHours.Checked) {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"hh\:mm\:ss");
+            }
+            else {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"mm\:ss");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            Properties.Settings.Default.Save();
+
+            notifyIcon1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            if (numHours.Value == 0 && numMinutes.Value == 0 && numSeconds.Value == 0) {
+                MessageBox.Show("Can't start a countdown with no time!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            countdown = TimeSpan.Zero;
+            countdownStarted = DateTime.Now;
+            countdown = TimeSpan.FromHours((double)numHours.Value) + TimeSpan.FromMinutes((double)numMinutes.Value) + TimeSpan.FromSeconds((double)numSeconds.Value);
+
+            toggleCountdown(true);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            //Properties.Settings.Default.timer_hours = numHours.Value;
+            //Properties.Settings.Default.timer_mins = numMinutes.Value;
+            //Properties.Settings.Default.timer_secs = numSeconds.Value;
+            //Properties.Settings.Default.filepath = txtFilePath.Text;
+
+            if (isCounting) {
+                DialogResult result = MessageBox.Show("The timer is still running, are you sure you want to exit?",
+                    "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+                if (result == DialogResult.No) {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            if (!isCounting) toggleCountdown(false);
+            TimeSpan currentValue = countdown - TimeSpan.FromTicks(DateTime.Now.Ticks - countdownStarted.Ticks);
+            if (currentValue <= TimeSpan.Zero) {
+                toggleCountdown(false);
+                currentValue = TimeSpan.Zero;
+                if (chkShowTray.Checked) {
+                    notifyIcon1.BalloonTipText = "Countdown has finished.";
+                    notifyIcon1.BalloonTipTitle = "Simple Countdown";
+                    notifyIcon1.ShowBalloonTip(5000);
+                }
+            }
+            try {
+
+                if (chkShowHours.Checked) {
+                    lblTime.Text = "Current Timer: " + currentValue.ToString(@"hh\:mm\:ss");
+                    File.WriteAllText(txtFilePath.Text, currentValue.ToString(@"hh\:mm\:ss"));
+                }
+                else {
+                    lblTime.Text = "Current Timer: " + currentValue.ToString(@"mm\:ss");
+                    File.WriteAllText(txtFilePath.Text, currentValue.ToString(@"mm\:ss"));
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("An error occured! See details\n" +
+                    ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                toggleCountdown(false);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e) {
+            toggleCountdown(false);
+        }
+
+        private void toggleCountdown(bool mode) {
+            numHours.Enabled = !mode;
+            numMinutes.Enabled = !mode;
+            numSeconds.Enabled = !mode;
+            txtFilePath.Enabled = !mode;
+            btnBrowse.Enabled = !mode;
+
+            btnStop.Enabled = mode;
+            btnStart.Enabled = !mode;
+            timer1.Enabled = mode;
+            isCounting = mode;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e) {
+            DialogResult result = saveFileDialog1.ShowDialog(ActiveForm);
+            if (result != DialogResult.OK) {
+                return;
+            }
+            txtFilePath.Text = saveFileDialog1.FileName;
+        }
+
+        private void num_ValueChanged(object sender, EventArgs e) {
+            countdown = TimeSpan.FromHours((double)numHours.Value) + TimeSpan.FromMinutes((double)numMinutes.Value) + TimeSpan.FromSeconds((double)numSeconds.Value);
+            if (chkShowHours.Checked) {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"hh\:mm\:ss");
+            }
+            else {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"mm\:ss");
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e) {
+            WindowState = FormWindowState.Normal;
+            Activate();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            if (WindowState == FormWindowState.Minimized && chkShowTray.Checked) {
+                ShowInTaskbar = false;
+            }
+            else {
+                ShowInTaskbar = true;
+            }
+        }
+
+        private void chkShowHours_CheckedChanged(object sender, EventArgs e) {
+            if (chkShowHours.Checked) {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"hh\:mm\:ss");
+            }
+            else {
+                lblTime.Text = "Current Timer: " + countdown.ToString(@"mm\:ss");
+            }
+        }
+    }
+}
